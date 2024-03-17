1894969cf744f6d10a64513b5755d901b20780f7
ansari2023 <imranahmedansari2021@gmail.com>
2024-03-18 02:40:35 +0500

    updated

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
 import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "enter your first number", type: "number", name: "firstNumber" },
+  { message: "enter second number", type: "number", name: "secondNumber"},
+  { message: "select one of the operators to parform action", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Maltiplication", "Division"],}
+]);
+// console.log(answer);
+//conditional statment
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Maltiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+  console.log("Please Select valid Operator");
+}
